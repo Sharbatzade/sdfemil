c4771991e349ec3c73b918e992d0cfd820ba86fc
amrah <esherbetzade@std.beu.edu.az>
2024-10-25 15:30:10 +0400

    1st

---
+++ src/main/java/com/example/emill/EmillApplication.java
@@ -0,0 +1,13 @@
+package com.example.emill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmillApplication.class, args);
+	}
+//first comment
+}
---
+++ src/test/java/com/example/emill/EmillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
